1e9989305e7588c124d1175f04e229b2aced9e83
kshku <1211shree@gmail.com>
2026-03-17 15:29:21 +0530

    setup google-genai

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-from config import GEMINI_API_KEY
-print(f"{GEMINI_API_KEY = }")
+from google import genai
+import config
+
+client = genai.Client(api_key=config.GEMINI_API_KEY)
+
+response = client.models.generate_content(
+        model=config.GEMINI_MODEL,
+        contents="Expalin how AI works in a few words",
+)
+
+print(f"{response.text}")
